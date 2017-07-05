2f49b774b0d196cb7835856df737757156a20a96
zhangli <sx-zhangl@dtdream.com>
2017-07-05 17:32:08 +0800

    修改了服务器端口号

--- server/index.js
+++ server/index.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-server.listen(3000, function(){
-  console.log('listening on 3000...');
+server.listen(3001, function(){
+  console.log('listening on 3001...');
 });
 
 io.on('connection', function (socket) {
